beedfaabdc00e17123c11a762a15b85f4d376582
Je-chan <jkyyc3@gmail.com>
2023-05-28 01:59:18 +0900

    FEAT : Header

--- kakao-friend-list/App.js
+++ kakao-friend-list/App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Platform, SafeAreaView, StyleSheet} from 'react-native';
+import Header from './src/Header';
+import {getStatusBarHeight, getBottomSpace} from "react-native-iphone-x-helper";
+
+
+const statusBarHeight = getStatusBarHeight(true);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: statusBarHeight,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-starts',
   },
 });
---
+++ kakao-friend-list/src/Header.js
@@ -0,0 +1,23 @@
+/** @format */
+import { View, Text, StyleSheet } from 'react-native';
+import {Ionicons} from '@expo/vector-icons'
+const IconBtn = (props) => {
+  return <View>
+    <Ionicons name={props.name} size={24} color="black"/>
+  </View>
+}
+
+export default () => {
+  return (
+    <View style={{flexDirection:"row", justifyContent: "space-between", paddingVertical: 10}}>
+      <Text style={{fontSize: 22, fontWeight: 'bold'}}>친구</Text>
+
+      <View style={{flexDirection: "row"}}>
+        <IconBtn name="search-outline"/>
+        <IconBtn name="person-add-outline"/>
+        <IconBtn name="md-musical-notes-outline"/>
+        <IconBtn name="ios-settings-outline"/>
+      </View>
+    </View>
+  );
+};
