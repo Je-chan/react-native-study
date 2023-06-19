ffc2245f799802e924427649e11b37c8daab6aa9
Je-chan <jkyyc3@gmail.com>
2023-06-20 00:26:55 +0900

    INIT : Phone background image project

---
+++ phone-background-image/src/components/TabIcon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Badge} from './Badge';
+import {Icon} from './Icons';
+
+export const TabIcon = (props)=>{
+	if(props.visibleBadge){
+		return (
+			<Badge fontSize={10}>
+				<Icon
+					name={this.props.iconName}
+					size={20}
+					color={'black'}/>
+			</Badge>
+		)
+	}
+
+	return (
+		<Icon
+			name={this.props.iconName}
+			size={20}
+			color={'black'}/>
+	)
+}
